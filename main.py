ffdf3c745bb53286282d1aecab41394b2f79c081
Narayana9290 <101434399+Narayana9290@users.noreply.github.com>
2023-05-03 20:26:39 +0000

    10

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ while running:
             explosionSound.play()
             bulletY = 500
             bullet_state = "ready"
-            score_value += 1
+            score_value += 10
             enemyX[i] = random.randint(0, 736)
             enemyY[i] = random.randint(50, 150)
 
